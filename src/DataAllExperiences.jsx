f104aa621b0701e1c7c802752906d9d76c1ababf
hansapradhan <pradhan.hansa@gmail.com>
2023-01-22 17:44:06 +0530

    added few more cards

--- src/DataAllExperiences.jsx
+++ src/DataAllExperiences.jsx
@@ -48,21 +48,21 @@ export default [
             rating: 3.0,
             reviewCount: 58
         },
-        location: "Sweden",
-        openSpots: 3,
+        location: "Maldives",
+        openSpots: 20,
     },
     {
         id: 5,
-        title: "Explore beautiful farm fresh market in italy",
+        title: "Explore beautiful farm fresh market in America",
         description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
         price: 60,
         coverImg: "carrots.jpg",
         stats: {
             rating: 3.6,
-            reviewCount: 23
+            reviewCount:56
         },
-        location: "Italy",
-        openSpots: 3,
+        location: "Texas",
+        openSpots: 30,
     },
     {
         id: 6,
@@ -72,9 +72,88 @@ export default [
         coverImg: "outdoorcafe.jpg",
         stats: {
             rating: 3.6,
-            reviewCount: 23
+            reviewCount: 38
         },
-        location: "Italy",
-        openSpots: 3,
+        location: "France",
+        openSpots: 40,
+    },
+    {
+        id: 7,
+        title: "Learn Still life photography with Zara Nolan",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 60,
+        coverImg: "cars.jpg",
+        stats: {
+            rating: 3.0,
+            reviewCount: 48
+        },
+        location: "Norway",
+        openSpots: 60,
+    },
+    {
+        id: 8,
+        title: "Learn Snow biking with with Edward Holmes",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 180,
+        coverImg: "snowbiking.jpg",
+        stats: {
+            rating: 5.0,
+            reviewCount: 100
+        },
+        location: "Alaska",
+        openSpots: 45,
+    },
+    {
+        id: 9,
+        title: "Explore the historical Places in London",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 60,
+        coverImg: "undergroundlondon.jpg",
+        stats: {
+            rating: 4.0,
+            reviewCount: 27
+        },
+        location: "London",
+        openSpots: 20,
+    },
+    {
+        id: 10,
+        title: "Explore the beautiful art exhibitions",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 80,
+        coverImg: "ArtGallery.jpg",
+        stats: {
+            rating: 4.3,
+            reviewCount: 48
+        },
+        location: "Paris",
+        openSpots: 83,
+    }
+    ,
+    {
+        id: 11,
+        title: "Learn boating in the great lakes",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 80,
+        coverImg: "boating.jpg",
+        stats: {
+            rating: 3.9,
+            reviewCount: 6
+        },
+        location: "Canada",
+        openSpots: 23,
+    },
+    {
+        id: 12,
+        title: "Explore marine life with Kesha",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 80,
+        coverImg: "turtle.jpg",
+        stats: {
+            rating: 4.6,
+            reviewCount: 3
+        },
+        location: "Canada",
+        openSpots: 66,
     }
 ]
